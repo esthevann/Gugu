0483acf04134e13be3a8d42e911ad63f66cc793e
Esthevan <esthevanferretti69@gmail.com>
2022-07-11 21:04:59 -0300

    profile page ui basics and bio field in schema

--- src/components/UserContent.tsx
+++ src/components/UserContent.tsx
@@ -12,8 +12,16 @@ interface UserContentProps {
 
 export default function UserContent(props: UserContentProps) {
     return (
-        <div>
-            <Image src={props.user.image!} alt={`${props.user.name} profile photo`}/>
+        <div className="flex flex-col flex-1 flex-grow pl-12 ">
+            <div className="flex items-center justify-center gap-2 p-6">
+                <Image src={props.user.image!} alt={`${props.user.name} profile photo`} 
+                    className="w-32 h-32 rounded-full"/>
+                <div >
+                    <h2 className="text-xl font-bold">{props.user.name} - {props.user.handle}</h2>
+                    <p className="w-[52rem] ">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum officiis sapiente voluptas facere accusantium fugiat cumque est, minus nostrum soluta optio hic expedita. Mollitia, dolore vitae ducimus at dolores necessitatibus.</p>
+                </div>
+            </div>
+            
             <Gugus gugus={props.user.Gugu}/>
         </div>
     )
